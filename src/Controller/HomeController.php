a15452cf1de04b3eff86241e1f4e62c839a2aa89
Francois Ly <ly.fongfrancois@gmail.com>
2022-10-19 16:54:29 +0200

    création home.html.twig et fonction home et redirection vers base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/base", name="base")
      */
     public function index(): Response
     {
-        return $this->render('base.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('base.html.twig');
+    }
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(): Response
+    {
+        $tableau = array(
+            "1" => array("id"=>1,"nom" => "Lebron", "prenom" => "James", "Number" => "0123456789"),
+            "2" => array("id"=>2,"nom" => "Lebron", "prenom" => "James", "Number" => "0123456789"),
+            "3" => array("id"=>3,"nom" => "Lebron", "prenom" => "James", "Number" => "0123456789")
+        );
+        return $this->render('home.html.twig', [
+            "contacts" => $tableau
         ]);
     }
 }
